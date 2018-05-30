de236506a3ef1727d74d82222e43c352b3eca638
leonemborg <leon0521@stud.kea.dk>
2018-05-30 09:36:46 +0200

    finnished client

--- Client.py
+++ Client.py
@@ -1,4 +1,24 @@
 import socket, threading
 
+#receiving method for messages
 def recvMessages(meSocket):
-    while True: 
+    while True:
+        data =  mySocket.recv(1024).decode()
+        print('Received from server: ' + data)
+        #changes to made 1-4
+
+host = 'localhost'
+port = 5000
+messages = ""
+
+mySocket = socket.socket()
+mySocket.connect((host,port))
+
+t = threading.Thread(target=recvMessages, args= (mySocket))
+t.start()
+
+while messages!= 'q':
+    messages = input(":")
+    mySocket.send(messages.encode())
+
+mySocket.close()
